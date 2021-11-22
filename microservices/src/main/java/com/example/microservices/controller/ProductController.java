93ba92cb108a923e06eaaa39c1bc0fb651bd7c9f
toilahtc96 <toilahtc@gmail.com>
2021-11-22 22:59:00 +0700

    first commit

---
+++ microservices/src/main/java/com/example/microservices/controller/ProductController.java
@@ -0,0 +1,53 @@
+package com.example.microservices.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.microservices.entities.Product;
+import com.example.microservices.entities.response.ProductInventoryResponse;
+import com.example.microservices.exceptions.ProductNotFoundException;
+import com.example.microservices.services.InventoryServiceClient;
+import com.example.microservices.services.ProductService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/products")
+public class ProductController {
+	private final ProductService productService;
+	private final InventoryServiceClient inventoryServiceClient;
+	Logger logger = LoggerFactory.logger(ProductController.class);
+
+	@Autowired
+	public ProductController(ProductService productService, InventoryServiceClient inventoryServiceClient) {
+		this.productService = productService;
+		this.inventoryServiceClient = inventoryServiceClient;
+	}
+
+	@GetMapping("")
+	public List<Product> allProducts() {
+		logger.debug("test call allProducts");
+		return productService.findAllProducts();
+	}
+
+	@GetMapping("/{code}")
+	public Product productByCode(@PathVariable String code) {
+		return productService.findByCode(code)
+				.orElseThrow(() -> new ProductNotFoundException("Product with code: [" + code + "] not found"));
+	}
+
+	@GetMapping("/inventory/{code}")
+	public Optional<ProductInventoryResponse> getProductInventoryByCode(@PathVariable String code) {
+		return inventoryServiceClient.getProductInventoryByCode(code);
+	}
+
+}
